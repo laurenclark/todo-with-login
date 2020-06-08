f8a38b55390a2c8a3a759f5cfc669b77afd6c8e0
Lauren <lauren.c4231@gmail.com>
2020-06-08 18:21:16 +0100

    Add header component stub

---
+++ src/components/UI/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <div>
+                <img src="" alt="" />
+                Logged in as John Doe
+                <div>
+                    <button>Logout</button>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;

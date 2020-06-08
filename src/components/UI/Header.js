955c0be4206046159384b5294f4a40c0f1e7c78a
Lauren <lauren.c4231@gmail.com>
2020-06-09 00:47:55 +0100

    Style header

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,14 +1,34 @@
+/** @jsx jsx */
 import React from 'react';
+import Button from './Button';
+import { jsx, css } from '@emotion/core';
 
 function Header() {
+    const headerStyle = css({
+        display: 'flex',
+        flexFlow: 'row-reverse',
+        padding: '5px 20px'
+    });
+
+    const divStyle = css({
+        display: 'flex',
+        alignItems: 'center',
+        '& p': {
+            margin: '0 10px'
+        }
+    });
+
     return (
-        <header>
-            <div>
-                <img src="" alt="" />
-                Logged in as John Doe
-                <div>
-                    <button>Logout</button>
-                </div>
+        <header css={headerStyle}>
+            <div css={divStyle}>
+                <img
+                    src="https://icon.now.sh/account_circle/48/9fadbb"
+                    alt=""
+                />
+                <p>
+                    Logged in as <strong>John Doe</strong>
+                </p>
+                <Button>Logout</Button>
             </div>
         </header>
     );

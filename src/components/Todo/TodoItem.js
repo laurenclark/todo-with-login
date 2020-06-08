f559a0f060ded44e1087448bce04afaea12f862f
Lauren <lauren.c4231@gmail.com>
2020-06-08 22:32:14 +0100

    Style and handle checkbox

--- src/components/Todo/TodoItem.js
+++ src/components/Todo/TodoItem.js
@@ -1,12 +1,21 @@
-import React from 'react';
+/** @jsx jsx */
+import React, { useState } from 'react';
+import { jsx } from '@emotion/core';
+import TodoItemStyle from './styles/TodoItem';
 
 function TodoItem() {
+    const [checked, setChecked] = useState(false);
+
+    const handleChange = () => {
+        setChecked((prev) => !prev);
+    };
+
     return (
-        <div>
-            <input type="checkbox" />
+        <li css={TodoItemStyle}>
+            <input type="checkbox" checked={checked} onChange={handleChange} />
             <p>Todo Item</p>
             <button>Delete</button>
-        </div>
+        </li>
     );
 }
 
---
+++ src/components/Todo/styles/TodoItem.js
@@ -0,0 +1,7 @@
+import { css } from '@emotion/core';
+
+const TodoItemStyle = css`
+    display: flex;
+`;
+
+export default TodoItemStyle;

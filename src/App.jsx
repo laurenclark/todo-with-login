22f5416aa001f172489e5dac528f68100c3ee17c
Lauren <lauren.c4231@gmail.com>
2020-06-08 18:22:17 +0100

    Setup basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import Todos from './components/Todo/Todos';
+import Login from './components/Login';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Switch>
+                <Route exact path="/">
+                    <Login />
+                </Route>
+
+                <Route path="/todos">
+                    <Todos />
+                </Route>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;

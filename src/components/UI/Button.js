5b4e89cefe82b88cf28b3b04571051e9dba851eb
Lauren <lauren.c4231@gmail.com>
2020-06-09 00:43:42 +0100

    Update button component

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -3,19 +3,19 @@ import React from 'react';
 import { jsx, css } from '@emotion/core';
 import { PropTypes } from 'prop-types';
 
-function Button({ children, bgColor, fgColor, size, hollow }) {
+function Button({ children, bgColor, fgColor, size, hollow, uniqueStyle }) {
     const ButtonStyle = css({
         borderRadius: '4px',
         background: `${hollow ? 'none' : bgColor}`,
         border: `${hollow ? '2px solid' : 'none'}`,
         borderColor: `${hollow ? bgColor : 'transparent'}`,
         color: `${hollow ? bgColor : fgColor}`,
-        padding: `${size === 'large' ? '1rem' : '0.6rem'}`,
+        padding: '10px',
         fontSize: `${size === 'large' ? '1.4rem' : '1rem'}`,
         cursor: 'pointer'
     });
 
-    return <button css={ButtonStyle}>{children}</button>;
+    return <button css={[ButtonStyle, uniqueStyle]}>{children}</button>;
 }
 
 Button.defaultProps = {
@@ -28,7 +28,8 @@ Button.propTypes = {
     bgColor: PropTypes.string,
     fgColor: PropTypes.string,
     hollow: PropTypes.bool,
-    size: PropTypes.string
+    size: PropTypes.string,
+    uniqueStyle: PropTypes.object
 };
 
 export default Button;

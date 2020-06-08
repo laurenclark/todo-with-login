1f67bc9350a8cec3e284688b533d7c8ba5d90709
Lauren <lauren.c4231@gmail.com>
2020-06-08 19:18:56 +0100

    Add datepicker component to todo form and format date

--- src/components/Todo/TodoForm.js
+++ src/components/Todo/TodoForm.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
 function TodoForm() {
+    const [theDate, setTheDate] = useState(new Date());
+    const options = {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric'
+    };
+
+    console.log(theDate.toLocaleDateString('en-GB', options));
+
     return (
         <form>
             <input type="text" placeholder="Wash dishes..." />
-            <input type="date" />
+            <DatePicker
+                dateFormat="dd/MM/yyyy"
+                selected={theDate}
+                onChange={(date) => setTheDate(date)}
+            />
             <button>Add Todo</button>
         </form>
     );

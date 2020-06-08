2e30bd596c1b606a86e1390ab2dcfa6da3389e2c
Lauren <lauren.c4231@gmail.com>
2020-06-08 18:21:29 +0100

    Add login component stub

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Login() {
+    return (
+        <form>
+            <input type="text" required />
+            <button>Login</button>
+        </form>
+    );
+}
+
+export default Login;

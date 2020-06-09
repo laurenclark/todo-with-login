0bc51aba3c7e98f513cbf6befef873e9a8b9bcad
Lauren <lauren.c4231@gmail.com>
2020-06-09 22:52:02 +0100

    Conditionally render header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Todos from './components/Todo/Todos';
 import Login from './components/Login';
 import { Switch, Route } from 'react-router-dom';
 import Header from './components/UI/Header';
+import { Context } from './Context';
 
 function App() {
+    const { username } = useContext(Context);
     return (
         <div className="App">
-            <Header />
+            {username && <Header username={username} />}
             <Switch>
                 <Route exact path="/">
                     <Login />
